6e231b7ccabc40ddc3a2c1567a310797ebc2c1a8
petergrutherford <petergrutherford88@gmail.com>
2025-10-20 11:21:28 -0700

    chain sync update

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub mod proofs;
 pub mod byzantine;
 pub mod dao;
 pub mod rewards;
+pub mod chain_evaluation;
 
 // Re-export commonly used types
 pub use types::*;
@@ -20,6 +21,7 @@ pub use engines::ConsensusEngine;
 pub use engines::enhanced_bft_engine::{EnhancedBftEngine, ConsensusStatus};
 pub use validators::{Validator, ValidatorManager};
 pub use proofs::*;
+pub use chain_evaluation::{ChainEvaluator, ChainDecision, ChainMergeResult, ChainSummary};
 
 #[cfg(feature = "dao")]
 pub use dao::*;
